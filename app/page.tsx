3666253076f5df635da1f669e07f791facc7beb7
Sandro Baccega <mail.sandro.baccega@gmail.com>
2024-10-21 15:16:53 +0200

    Add shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex h-full w-full justify-center bg-background pt-8">
-      <div className="w-full max-w-[var(--max-w)]">ok</div>
+      <div className="w-full max-w-[var(--max-w)]"></div>
     </main>
   );
 }
